84826146bad9cdc8f166be7443141828af882a0f
Kammradt <vinicius.kammradt1@gmail.com>
2020-12-21 14:56:23 -0300

    :sparkles: adds first version

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="rainbow-text">
+      PAULOCOACHI - SUA DOSE DE MAOTIVAÇÃO E COACH DIÁRIA
+    </h1>
+    <h2>{{ phrase }}</h2>
+    <img class="imgs" alt="Cuchi" src="./assets/cuchi.png" />
+
+    <h1 class="rainbow-text">Caso queira que o cuchi recite, clique aqui</h1>
+    <audio controls loop="true">
+      <source src="./assets/cuchi.mp3" type="audio/mpeg" codecs="vorbis" />
+    </audio>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    phrase: "",
+  }),
+  mounted() {
+    this.getPhrase();
+  },
+  methods: {
+    async getPhrase() {
+      const data = (await axios.get("https://type.fit/api/quotes")).data;
+      this.phrase = data[(data.length * Math.random()) | 0].text;
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +40,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: cornsilk
+}
+.imgs {
+  width: 100%;
+}
+.rainbow-text {
+  background-image: linear-gradient(
+    to left,
+    violet,
+    indigo,
+    blue,
+    green,
+    yellow,
+    orange,
+    red
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 </style>
